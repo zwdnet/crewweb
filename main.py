25ead3c31ba0d3d4620f3d0c26609e0d09b44dea
zym <zwdnet@163.com>
2016-04-01 06:59:31 +0800

    改进程序，用BeautifulSoup来查找网页内容，更改抓取网页的级数方式，使进入队列的链接都能被抓取。被某些网站屏蔽的问题仍未解决。
    Signed-off-by: zym <zwdnet@163.com>

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import jieba
 import time
+from bs4 import BeautifulSoup
 
 from collections import deque
 from Tools.Scripts.treesync import raw_input
@@ -25,7 +26,17 @@ cnt = 0
 output =  open("result.txt", "w+", encoding = "utf-8")
 linkCount = len(queue)
 
-while queue and cnt < maxCount:
+headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0',
+         'Accept':'	text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+         'Accept-Encoding':'gzip, deflate',
+         'Accept-Language':'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+         'Connection':'keep-alive',
+         #'Host':url,
+         #'Referer':url,
+         'X-Requested-With':'XMLHttpRequest'}
+bCrew = True
+
+while queue:
     url = queue.popleft()
     visited |= {url}
     print('已经抓取:' + str(cnt) + '  正在抓取 <----  ' + url + "队列链接数" + str(len(queue)))
@@ -33,6 +44,9 @@ while queue and cnt < maxCount:
 
     try:
         req = urllib.request.Request(url)# headers=webheader)
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0')
+        req.add_header('Accept-Language', 'zh-CN,zh')
+        #req.add_header('Referer', url)
         urlop = urllib.request.urlopen(req, timeout=5)
     except:
         print("抓取%s失败" % url)
@@ -43,18 +57,23 @@ while queue and cnt < maxCount:
     except:
         continue
 
-    linkre = re.compile("href=['\"]([^\"'>]*?)['\"].*?")
-    urlNum = 0
-    for x in linkre.findall(data):
-        if 'http' in x and x not in visited and urlNum < maxNum:
-            queue.append(x)
-            urlNum += 1
+    #爬链接，如果已爬到的链接没有超过maxCount并且之前也没超过,则抓取，否则，跳过。
+    if bCrew:
+        linkre = re.compile("href=['\"]([^\"'>]*?)['\"].*?")
+        urlNum = 0
+        for x in linkre.findall(data):
+            if 'http' in x and x not in visited and urlNum < maxNum:
+                queue.append(x)
+                urlNum += 1
+    if len(queue) > maxCount:
+        bCrew = False
+    soup = BeautifulSoup(data, "lxml")
+    data = soup.get_text()
     word = re.compile("[\u4e00-\u9fa5]+") #只爬中文
     for s in word.findall(data):
         output.write(s)
-
     #暂停一段时间，防止服务器负荷过重
-    time.sleep(0.01)
+    #time.sleep(0.01)
 
 print("结束")
 output.close()
@@ -106,6 +125,6 @@ p1 = np.poly1d(z)
 y1 = p1(x)
 
 #绘图
-p1 = plt.plot(x, y)
+p1 = plt.plot(x, y, 'r*')
 p2 = plt.plot(x, y1)
 plt.show()
